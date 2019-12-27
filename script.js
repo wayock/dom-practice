ae8f09306780021ff9f403515b1372476e216bb1
Matthew Wayock <wayock@gmail.com>
2019-12-27 13:19:20 -0500

    dom practice

---
+++ script.js
@@ -0,0 +1,39 @@
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.getElementsByTagName("li")
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+ul.onclick=function(event){
+	var target=event.target;
+	console.log(event.target);
+	target.classList.toggle("done");
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
